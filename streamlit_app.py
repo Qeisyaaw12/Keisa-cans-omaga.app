82a45bb51cfbfc878150a9594b987c2b5c64d6dd
Qeisyaaw12 <shandraqeisya12@gmail.com>
2025-05-15 14:28:59 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,9 @@ import streamlit as st
 
 st.title("🎈 asik aku buat gim")
 st.write(kesa awokawok)
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+st.image("views/IMG-20250215-WA0034.jpg", width=200)
+st.write("\n")
+st.subheader("Qeisya Shandra N")
+st.image(
+ "Mari latihan membuat website melalui Github dan Streamlit"
 )
-
-st.image("views/IMG-20250215-WA0034.jpg")
